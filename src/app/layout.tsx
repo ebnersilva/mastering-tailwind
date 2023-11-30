e7cbee6809d206a2b9e4073750ab3a9a1567637e
ebner <ebner.suporte@hotmail.com>
2023-11-30 14:54:58 -0300

    feat: add antialiased flag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="antialiased">
       <body>
         <div className="grid min-h-screen grid-cols-app">
           <Sidebar />
